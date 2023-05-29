ad7503063ab601fc7d3966f300675afcdc7a3d2f
Evan Wimpey <evan.wimpey@elderresearch.com>
2023-05-28 21:58:04 -0400

    updating formatting functions

--- R/batter_viz_func.R
+++ R/batter_viz_func.R
@@ -3,9 +3,9 @@
 #'
 #' @param tm Character value indicating the team.
 #' @param yr A numeric value indicating the four digit year.
-#' @return A character vector of player_ids
+#' @return A tibble with player name and id, and wRC
 #' @export
-get_top_batters <- function(tm, yr) {
+get_top_batters <- function(tm, yr, num_players = 7) {
   # Find top wRC players on a team
   all_batters <- baseballr::fg_batter_leaders(x = yr, y = yr, qual = 0)
   
@@ -14,15 +14,13 @@ get_top_batters <- function(tm, yr) {
     filter(Team == tm) %>%
     select(playerid, Name, wRC) %>%
     arrange(wRC, decreasing = TRUE) %>%
-    top_n(num_players) %>%
-    pull(playerid)
+    top_n(num_players, wt = wRC) %>%
+    select(Name, playerid)
   
   return(team_top)
 }
 
-#' Add two numbers
-#'
-#' This function adds two numbers and returns the result.
+#' This function gets the daily wRC for a given batter for a given year.
 #'
 #' @param player_id A character value representing a fangraphs playerid.
 #' @param yr A numeric value indicating four digit year.
@@ -35,7 +33,7 @@ get_player_daily_wrc <- function(player_id, yr){
     group_by(Date) %>%
     summarise(wRC = mean(wRC))
   
-  player_wrc$player <- player_id
+  player_wrc$playerid <- player_id
   
   return(player_wrc)
 }
@@ -43,14 +41,21 @@ get_player_daily_wrc <- function(player_id, yr){
 #' Get dataframe of individual players' wRC by date
 #'
 #'
-#' @param team_top a vector of player_id values
+#' @param team_top tibble with player_ids
 #' @param y A numeric value representing the four digit year
 #' @return A tibble with daily wRC for multiple players
 #' @export
-add_numbers <- function(team_top, yr) {
+get_daily_wrcs <- function(team_top, yr) {
   
-  m = lapply(team_top, get_player_daily_wrc, year=yr)
-  tm_wrc_by_date <- do.call(rbind, m)
+  # get vector of player ids
+  player_ids <- unique(team_top$playerid)
+  
+  # get daily wRC for each player
+  m = lapply(player_ids, get_player_daily_wrc, year=yr)
+  
+  # get dataframe of all players by day
+  tm_wrc_by_date <- do.call(rbind, m) %>%
+    left_join(., team_top, by="playerid")
   
   return(yr)
 }
